6bbdb1d2cfc697ff19817efe8b2396d756f477f5
Saad Siddiqui <saadsiddiqui.ned16@gmail.com>
2019-08-03 17:56:47 +0500

    Add MATLAB code for generating videos.

---
+++ MATLAB Aliasing Assignment/sampling_test.m
@@ -0,0 +1,9 @@
+%% EE-394 Digital Signal Processing - Bonus Task 1 
+% Saad Mashkoor Siddiqui, EE-16163, Section D, TE-EE, Fall 2018
+% Test File - sampling_test.m
+% Tests function generate_movie.m
+
+%% Script
+
+% args - fps, rotation speed, title, duration
+generate_movie( 30, 10, 'Oversampling', 5 );
